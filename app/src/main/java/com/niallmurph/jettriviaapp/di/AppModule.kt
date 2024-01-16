43bf1ea066c1b4f69e352c6193aba627810bd8e6
Niall <niall@Aarons-MacBook-Air.local>
2024-01-16 11:18:06 +0000

    added material from videos, fixed failing gradle build

---
+++ app/src/main/java/com/niallmurph/jettriviaapp/di/AppModule.kt
@@ -0,0 +1,31 @@
+package com.niallmurph.jettriviaapp.di
+
+import com.niallmurph.jettriviaapp.network.QuestionApi
+import com.niallmurph.jettriviaapp.repository.QuestionRepository
+import com.niallmurph.jettriviaapp.util.Constants
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun providesQuestionRepository(api : QuestionApi) = QuestionRepository(api)
+
+    @Singleton
+    @Provides
+    fun provideQuestionApi(): QuestionApi{
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(QuestionApi::class.java)
+    }
+}
