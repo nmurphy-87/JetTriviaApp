0372cb621e2aa1e7e88936324994eea8d7ff61a8
Niall <niall@Aarons-MacBook-Air.local>
2024-01-16 15:55:32 +0000

    Added tracker composable for top of screen

--- app/src/main/java/com/niallmurph/jettriviaapp/component/Questions.kt
+++ app/src/main/java/com/niallmurph/jettriviaapp/component/Questions.kt
@@ -1,9 +1,24 @@
 package com.niallmurph.jettriviaapp.component
 
 import android.util.Log
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.ParagraphStyle
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextIndent
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.niallmurph.jettriviaapp.screens.QuestionsViewModel
+import com.niallmurph.jettriviaapp.util.AppColours
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 
@@ -22,4 +37,61 @@ fun Questions(viewModel: QuestionsViewModel) {
         }
     }
     Log.d("Q_LIST_SIZE", "Size = ${questions?.size}")
+}
+
+@Preview
+@Composable
+fun QuestionDisplay(){
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .padding(4.dp),
+        color = AppColours.mDarkPurple
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(12.dp),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.Start
+        ){
+            QuestionTracker()
+        }
+    }
+}
+
+@Preview
+@Composable
+fun QuestionTracker(
+    counter : Int = 10,
+    listSize : Int = 100
+){
+    Text(
+        text = buildAnnotatedString {
+            withStyle(style = ParagraphStyle(
+                textIndent = TextIndent.None
+            )) {
+                withStyle(
+                    style = SpanStyle(
+                        color = AppColours.mLightGray,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 24.sp
+                    )
+                ) {
+                    append("Question $counter /")
+
+                    withStyle(
+                        style = SpanStyle(
+                            color = AppColours.mLightGray,
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 12.sp
+                        )
+                    ) {
+                        append(" $listSize")
+                    }
+                }
+            }
+        },
+        modifier = Modifier.padding(18.dp)
+    )
 }
