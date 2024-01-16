66744708fbfb1b867a2b6eafb2051a2f28f80550
Niall <niall@Aarons-MacBook-Air.local>
2024-01-16 11:54:23 +0000

    added to MainActivity and Manifest

--- app/src/main/java/com/niallmurph/jettriviaapp/MainActivity.kt
+++ app/src/main/java/com/niallmurph/jettriviaapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.niallmurph.jettriviaapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,8 +11,14 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.niallmurph.jettriviaapp.screens.QuestionsViewModel
 import com.niallmurph.jettriviaapp.ui.theme.JetTriviaAppTheme
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,7 +29,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    TriviaHome()
                 }
             }
         }
@@ -30,14 +37,29 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun TriviaHome(viewModel : QuestionsViewModel = hiltViewModel()){
+    Questions(viewModel)
+}
+
+@Composable
+fun Questions(viewModel: QuestionsViewModel) {
+    val questions = viewModel.data.value.data?.toMutableList()
+    runBlocking {
+        delay(1000)
+    }
+    if(viewModel.data.value.loading == true){
+        Log.d("Q_LIST_SIZE", "Size = Loading")
+    } else {
+        questions?.forEach {
+            Log.d("QUESTION", it.question)
+        }
+    }
+    Log.d("Q_LIST_SIZE", "Size = ${questions?.size}")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JetTriviaAppTheme {
-        Greeting("Android")
     }
 }
